3d8e7be17d5b60bab64ff87226c7a8c88f9be95f
Etanto Nuari Tidar <etantonuari@gmail.com>
2022-03-18 23:36:50 +0700

    first try

---
+++ I am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I am Poor
+//
+//  Created by Etanto Nuari Tidar on 22/03/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
